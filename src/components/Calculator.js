1bc853abfc23a0ac806eb7a20796e5587dac7c92
Abhinav Kumar Gupta <84512702+alhad-balak@users.noreply.github.com>
2022-05-06 22:16:07 +0530

    Calculator styling done.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import './Calculator.css'
 
 const Calculator = () => {
-    const [inputData, setInputData] = useState('');
+    const [inputData, setInputData] = useState('sdhcv');
     const [outputData, setOutputData] = useState(0);
 
     const handleClick=(event)=> {
@@ -22,33 +22,33 @@ const Calculator = () => {
             </div>
             <div className="numpad">
                 <div className="row">
-                    <button className="c num-btn" onClick={handleClick('asjjh')}>C</button>
-                    <button className="c num-btn">+/-</button>
-                    <button className="c num-btn">%</button>
-                    <button className="c num-btn">/</button>
+                    <button className="c num-btn" id='clear-btn'>C</button>
+                    <button className="c num-btn opr0-btn">+/-</button>
+                    <button className="c num-btn opr0-btn">%</button>
+                    <button className="c num-btn opr-btn">/</button>
                 </div>
                 <div className="row">
                     <button className="c num-btn">7</button>
                     <button className="c num-btn">8</button>
                     <button className="c num-btn">9</button>
-                    <button className="c num-btn">x</button>
+                    <button className="c num-btn opr-btn">x</button>
                 </div>
                 <div className="row">
                     <button className="c num-btn">4</button>
                     <button className="c num-btn">5</button>
                     <button className="c num-btn">6</button>
-                    <button className="c num-btn">-</button>
+                    <button className="c num-btn opr-btn">-</button>
                 </div>
                 <div className="row">
                     <button className="c num-btn">1</button>
                     <button className="c num-btn">2</button>
                     <button className="c num-btn">3</button>
-                    <button className="c num-btn">+</button>
+                    <button className="c num-btn opr-btn">+</button>
                 </div>
                 <div className="row">
                     <button className="c num-btn" id="zero-btn">0</button>
-                    <button className="c num-btn">.</button>
-                    <button className="c num-btn">=</button>
+                    <button className="c num-btn opr-btn">.</button>
+                    <button className="c num-btn" id='equ-btn'>=</button>
                 </div>
             </div>
         </div>
