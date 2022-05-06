e71ec37a5d06611f41f42001339dda78a4dd1a87
Abhinav Kumar Gupta <84512702+alhad-balak@users.noreply.github.com>
2022-05-06 18:39:24 +0530

    Styling under-proces

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,47 +6,52 @@ const Calculator = () => {
     const [inputData, setInputData] = useState('');
     const [outputData, setOutputData] = useState(0);
 
-    const handleClick = (event) => {
+    const handleClick=(event)=> {
         console.log(event);
     }
+
+    function handleClear() {
+        setInputData('');
+        setOutputData(0);
+    }
     return (
-        <>
+        <div className='calculator-body'>
             <div className="display">
                 <div className="input">{inputData}</div>
                 <div className="output">={outputData}</div>
             </div>
             <div className="numpad">
                 <div className="row">
-                    <div className="c num-btn" onClick={handleClick("aggh")}>C</div>
-                    <div className="c num-btn">+/-</div>
-                    <div className="c num-btn">%</div>
-                    <div className="c num-btn">/</div>
+                    <button className="c num-btn" onClick={handleClick('asjjh')}>C</button>
+                    <button className="c num-btn">+/-</button>
+                    <button className="c num-btn">%</button>
+                    <button className="c num-btn">/</button>
                 </div>
                 <div className="row">
-                    <div className="c num-btn">7</div>
-                    <div className="c num-btn">8</div>
-                    <div className="c num-btn">9</div>
-                    <div className="c num-btn">x</div>
+                    <button className="c num-btn">7</button>
+                    <button className="c num-btn">8</button>
+                    <button className="c num-btn">9</button>
+                    <button className="c num-btn">x</button>
                 </div>
                 <div className="row">
-                    <div className="c num-btn">4</div>
-                    <div className="c num-btn">5</div>
-                    <div className="c num-btn">6</div>
-                    <div className="c num-btn">-</div>
+                    <button className="c num-btn">4</button>
+                    <button className="c num-btn">5</button>
+                    <button className="c num-btn">6</button>
+                    <button className="c num-btn">-</button>
                 </div>
                 <div className="row">
-                    <div className="c num-btn">1</div>
-                    <div className="c num-btn">2</div>
-                    <div className="c num-btn">3</div>
-                    <div className="c num-btn">+</div>
+                    <button className="c num-btn">1</button>
+                    <button className="c num-btn">2</button>
+                    <button className="c num-btn">3</button>
+                    <button className="c num-btn">+</button>
                 </div>
                 <div className="row">
-                    <div className="c num-btn" id="zero-btn">0</div>
-                    <div className="c num-btn">.</div>
-                    <div className="c num-btn">=</div>
+                    <button className="c num-btn" id="zero-btn">0</button>
+                    <button className="c num-btn">.</button>
+                    <button className="c num-btn">=</button>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
