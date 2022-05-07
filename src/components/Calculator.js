f9fa5137e4bdd6dc6f6efa21c5951ef3f9f56918
Abhinav Kumar Gupta <84512702+alhad-balak@users.noreply.github.com>
2022-05-07 14:04:33 +0530

    Logic part completed

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,16 +3,113 @@ import { useState } from 'react';
 import './Calculator.css'
 
 const Calculator = () => {
-    const [inputData, setInputData] = useState('sdhcv');
+    const [inputData, setInputData] = useState('');
     const [outputData, setOutputData] = useState(0);
+    const [prevNum, setPrevNum] = useState(0);
+    const [currNum, setCurrNum] = useState(0);
+    const [prevOpr, setPrevOpr] = useState(null);
+    const [plusMinus, setPlusMinus] = useState('+');
 
-    const handleClick=(event)=> {
-        console.log(event);
+
+    const handleNum = (event) => {
+        // console.log(event.target.value);
+        setInputData(inputData + event.target.value);
+        let num;
+        if (currNum === 0) {
+            num = event.target.value;
+            setCurrNum(Number(num))
+            // setInputData(outputData);
+        }
+        else {
+            num = String(currNum) + event.target.value;
+            setCurrNum(Number(num));
+        }
+
+        // if (prevOpr === '+') {
+        //     let cal = prevNum + currNum;
+        //     setOutputData(cal);
+        // }
+        // if (prevOpr === '-') {
+        //     let cal = prevNum - currNum;
+        //     setOutputData(cal);
+        // }
+        // if (prevOpr === '*') {
+        //     let cal = prevNum * currNum;
+        //     setOutputData(cal);
+        // }
+        // if (prevOpr === '/') {
+        //     let cal = prevNum / currNum;
+        //     setOutputData(cal);
+        // }
+        // if (prevOpr === '%') {
+        //     let cal = currNum / 100;
+        //     setOutputData(cal);
+        // }
+        // console.log((currNum));
     }
 
-    function handleClear() {
+    const handleOpr = (event) => {
+        // console.log(prevNum);
+        if (inputData === '') {
+            setInputData(outputData + event.target.value);
+            // setPrevNum(outputData)
+        }
+        else {
+            setInputData(inputData + event.target.value);
+            setPrevNum(currNum);
+            setCurrNum(0);
+        }
+        setPrevOpr(event.target.value);
+        if (prevOpr !== null && currNum === 0)
+            setOutputData('invalid!');
+
+    }
+
+    const handlePlusMinus = (event) => {
+        if (plusMinus === '+')
+            setPlusMinus('-');
+        else
+            setPlusMinus('+');
+        setCurrNum(0 - currNum);
+        // console.log(event.target.value);
+    }
+
+    function handleClear(event) {
         setInputData('');
         setOutputData(0);
+        setCurrNum(0);
+        setPrevNum(0);
+        // console.log(event.target.value);
+    }
+    const handleCalculate = (event) => {
+        let cal;
+        if (prevOpr === null)
+            setOutputData(Number(inputData));
+        if (prevOpr === '+') {
+            cal = prevNum + currNum;
+            setOutputData(cal);
+        }
+        if (prevOpr === '-') {
+            cal = prevNum - currNum;
+            setOutputData(cal);
+        }
+        if (prevOpr === '*') {
+            cal = prevNum * currNum;
+            setOutputData(cal);
+        }
+        if (prevOpr === '/') {
+            cal = prevNum / currNum;
+            setOutputData(cal);
+        }
+        if (prevOpr === '%') {
+            cal = currNum / 100;
+            setOutputData(cal);
+        }
+        setCurrNum(0)
+        setPrevNum(cal);
+        // setPrevNum(outputData);
+        setInputData('');
+        setPrevOpr(null)
     }
     return (
         <div className='calculator-body'>
@@ -22,33 +119,33 @@ const Calculator = () => {
             </div>
             <div className="numpad">
                 <div className="row">
-                    <button className="c num-btn" id='clear-btn'>C</button>
-                    <button className="c num-btn opr0-btn">+/-</button>
-                    <button className="c num-btn opr0-btn">%</button>
-                    <button className="c num-btn opr-btn">/</button>
+                    <button className="c num-btn" id='clear-btn' value='clear' onClick={handleClear}>C</button>
+                    <button className="c num-btn opr0-btn" value='+/-' onClick={handlePlusMinus}>+/-</button>
+                    <button className="c num-btn opr0-btn" value='%' onClick={handleOpr}>%</button>
+                    <button className="c num-btn opr-btn" value='/' onClick={handleOpr}>/</button>
                 </div>
                 <div className="row">
-                    <button className="c num-btn">7</button>
-                    <button className="c num-btn">8</button>
-                    <button className="c num-btn">9</button>
-                    <button className="c num-btn opr-btn">x</button>
+                    <button className="c num-btn" value='7' onClick={handleNum}>7</button>
+                    <button className="c num-btn" value='8' onClick={handleNum}>8</button>
+                    <button className="c num-btn" value='9' onClick={handleNum}>9</button>
+                    <button className="c num-btn opr-btn" value='*' onClick={handleOpr}>x</button>
                 </div>
                 <div className="row">
-                    <button className="c num-btn">4</button>
-                    <button className="c num-btn">5</button>
-                    <button className="c num-btn">6</button>
-                    <button className="c num-btn opr-btn">-</button>
+                    <button className="c num-btn" value='4' onClick={handleNum}>4</button>
+                    <button className="c num-btn" value='5' onClick={handleNum}>5</button>
+                    <button className="c num-btn" value='6' onClick={handleNum}>6</button>
+                    <button className="c num-btn opr-btn" value='-' onClick={handleOpr}>-</button>
                 </div>
                 <div className="row">
-                    <button className="c num-btn">1</button>
-                    <button className="c num-btn">2</button>
-                    <button className="c num-btn">3</button>
-                    <button className="c num-btn opr-btn">+</button>
+                    <button className="c num-btn" value='1' onClick={handleNum}>1</button>
+                    <button className="c num-btn" value='2' onClick={handleNum}>2</button>
+                    <button className="c num-btn" value='3' onClick={handleNum}>3</button>
+                    <button className="c num-btn opr-btn" value='+' onClick={handleOpr}>+</button>
                 </div>
                 <div className="row">
-                    <button className="c num-btn" id="zero-btn">0</button>
-                    <button className="c num-btn opr-btn">.</button>
-                    <button className="c num-btn" id='equ-btn'>=</button>
+                    <button className="c num-btn" id="zero-btn" value='0' onClick={handleNum}>0</button>
+                    <button className="c num-btn opr-btn" value='.' onClick={handleNum}>.</button>
+                    <button className="c num-btn" id='equ-btn' value='=' onClick={handleCalculate}>=</button>
                 </div>
             </div>
         </div>
